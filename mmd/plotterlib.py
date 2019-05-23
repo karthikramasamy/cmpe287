c041ce6a753989b672d4be7c5689ccb284812a14
Karthik Ramasamy <karthira@cisco.com>
2019-05-22 18:22:16 -0700

    flake8 fixes, formatting

--- mmd/plotterlib.py
+++ mmd/plotterlib.py
@@ -2,20 +2,21 @@ from tabulate import tabulate
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 class OpCodePlotter:
-    
+
     def draw_opcode_matrix(self, matrix, headers):
         table = tabulate(matrix, headers, tablefmt="fancy_grid")
-        print(table)    
-        
+        print(table)
+
     def draw_opcode_graph(self, adjacency_matrix, unique_opcode_list):
         labels = self.make_label_dict(unique_opcode_list)
         G = nx.MultiDiGraph(adjacency_matrix)
-        edge_labels = dict( ((u, v), d["weight"]) for u, v, d in G.edges(data=True) )
+        edge_labels = dict(((u, v), d["weight"]) for u, v, d in G.edges(data=True))
         pos = nx.spring_layout(G)
         nx.draw(G, pos)
         nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-        nx.draw(G,pos,node_size=1000, labels=labels, with_labels=True)
+        nx.draw(G, pos, node_size=1000, labels=labels, with_labels=True)
         plt.show()
 
     def make_label_dict(self, labels):
@@ -29,8 +30,8 @@ if __name__ == "__main__":
     import numpy as np
     plotter = OpCodePlotter()
     opcode_list = ['add', 'call', 'jmp', 'nop', 'sub']
-    opcode_matrix = np.around(np.random.rand(5,5), 2)
-    
+    opcode_matrix = np.around(np.random.rand(5, 5), 2)
+
     print("\n Node List: \n", opcode_list)
     print("\n Matrix: \n")
     plotter.draw_opcode_matrix(opcode_matrix, opcode_list)
--- tests/test_plotterlib.py
+++ tests/test_plotterlib.py
@@ -2,20 +2,22 @@ import unittest
 import numpy as np
 from mmd.plotterlib import OpCodePlotter
 
+
 class PlotterLibTests(unittest.TestCase):
-    
+
     def test_draw_opcode_matrix(self):
         plotter = OpCodePlotter()
         opcode_list = ['add', 'call', 'jmp', 'nop', 'sub']
-        opcode_matrix = np.around(np.random.rand(5,5), 2)
+        opcode_matrix = np.around(np.random.rand(5, 5), 2)
         plotter.draw_opcode_matrix(opcode_matrix, opcode_list)
-        
+
     def test_draw_opcode_graph(self):
         plotter = OpCodePlotter()
         opcode_list = ['add', 'call', 'jmp', 'nop', 'sub']
-        opcode_matrix = np.around(np.random.rand(5,5), 2)
+        opcode_matrix = np.around(np.random.rand(5, 5), 2)
         #plotter.draw_opcode_matrix(opcode_matrix, opcode_list)
         plotter.draw_opcode_graph(opcode_matrix, opcode_list)
-    
+
+
 if __name__ == '__main__':
     unittest.main()
